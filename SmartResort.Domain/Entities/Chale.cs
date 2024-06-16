21485ec1af281e1e04343347c68cdf630e8851d6
danilo.silva <danilo.silva@msn.com>
2024-06-16 01:07:58 -0300

    Definindo o Modelo Chale

--- SmartResort.Domain/Entities/Chale.cs
+++ SmartResort.Domain/Entities/Chale.cs
@@ -1,13 +1,29 @@
-﻿namespace SmartResort.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartResort.Domain.Entities;
 
 public class Chale
 {
     public int Id { get; set; }
+
+    [MaxLength(50)]
     public required string Nome { get; set; }
+
+    [Display(Name = "Descrição")]
     public string? Descricao { get; set; }
+
+    [Display(Name = "Preço por Diária")]
+    [Range(10, 10000)]
     public decimal Preco { get; set; }
+
+    [Display(Name = "Tamanho em m²")]
     public int MetrosQuadrados { get; set; }
+
+    [Display(Name = "Ocupação")]
+    [Range(1, 10)]
     public int Ocupacao { get; set; }
+
+    [Display(Name = "URL da Imagem")]
     public string? UrlImagem { get; set; }
     public DateTime DataCriacao { get; set; }
     public DateTime? DataAtualizacao { get; set; }
---
+++ SmartResort.Web/Controllers/ChalesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartResort.Domain.Entities;
+using SmartResort.Infrastructure.Data;
+
+namespace SmartResort.Web.Controllers;
+
+public class ChalesController(SmartResortContext context) : Controller
+{
+    private readonly SmartResortContext _context = context;
+
+    public IActionResult Index()
+    {
+        var chales = _context.Chales.ToList();
+        return View(chales);
+    }
+
+    public IActionResult Criar()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Criar(Chale chale)
+    {
+        if (chale.Nome == chale.Descricao)
+        {
+            ModelState.AddModelError("Nome", "O nome do chalé não pode ser igual à descrição.");
+        }
+        if (ModelState.IsValid)
+        {
+            _context.Chales.Add(chale);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+}
